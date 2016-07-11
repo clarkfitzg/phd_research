fa3a1a848ccbf0679301c97f18ff81110b722c28
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-07-11 10:03:18 -0700

    clarifying some questions

--- ddR/testspark2.R
+++ ddR/testspark2.R
@@ -12,16 +12,28 @@ iris_tbl = copy_to(sc, iris, overwrite=TRUE)
 # Now can we manipulate this at some lower level?
 library(sparkapi)
 
+# Class is "spark_jobj"
 iris_low = spark_dataframe(iris_tbl)
 
 # Following example function in sparkapi docs
 
+# This is pretty darn far from passing in arbitrary R code
+billionBigInteger <- invoke_new(sc, "java.math.BigInteger", "1000000000")
+class(billionBigInteger)
+
 # So it appears the ... in the invoke functions pass arguments into the
 # Java objects. Which is the natural thing to do.
-invoke_static(sc, "java.lang.Math", "hypot", 10, 20) 
+a = invoke_static(sc, "java.lang.Math", "hypot", 10, 20) 
 
 # Doesn't work
 invoke(iris_low, "select", "Sepal_Length")
+
+# So this is the experimental Dataset class from Spark 1.6
+# NOT an RDD. But probably pretty similar.
+# http://spark.apache.org/docs/latest/api/java/index.html?org/apache/spark/sql/Dataset.html
+invoke(iris_low, "getClass")
+
+invoke(iris_low, "schema")
 
 # From error log:
 # 16/07/08 16:45:25 WARN RBackendHandler: select(class org.apache.spark.sql.TypedColumn,class org.apache.spark.sql.TypedColumn)
@@ -33,3 +45,12 @@ invoke(iris_low, "select", "Sepal_Length")
 invoke(iris_low, "printSchema")
 
 # hc = hive_context(sc)
+
+# Let's convert it to the more stable and familiar data frame
+iris_df = invoke(iris_low, "toDF")
+
+# Still a Dataset
+invoke(iris_df, "getClass")
+
+# This method of writing it should be equivalent.
+invoke(iris_low, "toDF") %>% invoke("getClass")
