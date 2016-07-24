f26a98cf745571a5fe9305dc937cd04ca5c9e284
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-07-24 13:18:45 -0700

    more experiments with dapplyCollect

---
+++ ddR/spark/sparkR_dapplyCollect7.R
@@ -0,0 +1,21 @@
+# Sun Jul 24 07:39:35 PDT 2016
+
+library("SparkR")
+
+n = 3
+df = data.frame(key = 1:n)
+df$value = lapply(letters[1:n], serialize, connection = NULL)
+
+sparkR.stop()
+sc <- sparkR.init()
+sqlContext <- sparkRSQL.init(sc)
+spark_df = createDataFrame(sqlContext, df_rawvals)
+
+df2 = data.frame(a=1:n, b = letters[1:n])
+
+spark_df2 = createDataFrame(sqlContext, df2)
+dapplyCollect(spark_df2, function(x) x)
+
+dapplyCollect(spark_df, function(x) x)
+
+dapplyCollect(spark_df, function(x) x)
