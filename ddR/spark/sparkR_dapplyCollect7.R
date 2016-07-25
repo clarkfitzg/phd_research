32e5ec4151672fb844e82bef0bc5c568dc8e3c14
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-07-25 15:30:34 -0700

    fix example for Shivaram

--- ddR/spark/sparkR_dapplyCollect7.R
+++ ddR/spark/sparkR_dapplyCollect7.R
@@ -9,13 +9,12 @@ df$value = lapply(letters[1:n], serialize, connection = NULL)
 sparkR.stop()
 sc <- sparkR.init()
 sqlContext <- sparkRSQL.init(sc)
-spark_df = createDataFrame(sqlContext, df_rawvals)
+spark_df = createDataFrame(sqlContext, df)
+
+# Fails
+#dapplyCollect(spark_df, function(x) x)
 
 df2 = data.frame(a=1:n, b = letters[1:n])
-
 spark_df2 = createDataFrame(sqlContext, df2)
+# Works fine
 dapplyCollect(spark_df2, function(x) x)
-
-dapplyCollect(spark_df, function(x) x)
-
-dapplyCollect(spark_df, function(x) x)
