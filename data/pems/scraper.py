3f80df7dc652d4ce3bd86062e890f8e0f29ed878
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-10-13 17:49:49 -0700

    need to change this to take command line arguments

--- data/pems/scraper.py
+++ data/pems/scraper.py
@@ -48,14 +48,17 @@ def download(links, cookies, datadir, chunk_size=10240):
 
 if __name__ == "__main__":
 
+    # TODO: clean this up and have the script take all these as command
+    # line args
+
     # Copy your cookie from the web browser and save it in this file
     with open("cookie.txt") as f:
         cookies = {"PHPSESSID": f.read().strip()}
 
-    links = pems_links("dist3_raw30sec_13oct16.html")
+    links = pems_links("dist4_raw30sec_13oct16.html")
 
     # Useful to test locally with just 2 before full download
     #links = dict((links.popitem(), links.popitem()))
 
-    datadir = "/scratch/clarkf/pems/district3/"
+    datadir = "/scratch/clarkf/pems/district4/"
     download(links, cookies, datadir=datadir)
