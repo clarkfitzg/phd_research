0c273c578a401b3e5ef48a54870ca9012aad53d8
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-10-17 16:53:41 -0700

    Going to run filtering script on poisson

--- data/pems/I80filter.py
+++ data/pems/I80filter.py
@@ -5,27 +5,54 @@ Script to filter the huge amount of PeMS data into just those detectors on
 I80 around Davis, CA.
 """
 
+import os
 import gzip
 
-
-fname = "/home/clark/data/pems/d04_text_station_raw_2016_04_13.txt.gz"
 
 with open("davis_80_station_ids.csv") as f:
     station_ids = set(x.strip() for x in f)
 
 
-def lines(gzfile, pattern_set=station_ids):
+def matchlines(gzfile, pattern_set=station_ids):
     """
     Generate the lines matching the pattern_set
     """
     with gzip.open(gzfile, mode="rt") as f:
         for line in f:
+            # Data format is
+            # timestamp, station ID, ...
             station = line.split(",")[1]
             if station in pattern_set:
                 yield line
 
 
+def main(args):
 
-I80_fname = "~/data/pems/I80_davis.txt.gz"
-with gzip.open(I80_fname, mode="at") as f:
-    addfile(fname)
+    fnames = (os.sep.join((args.datadir, x)) for x in os.listdir(args.datadir)
+                if x.endswith(".txt.gz"))
+
+    with gzip.open(args.I80_fname, mode="at") as outfile:
+        for f in fnames:
+            for l in matchlines(f):
+                outfile.write(l)
+
+
+# sanity check - works!
+#I80 = read_station_raw(I80_fname)
+#I80["ID"].value_counts()
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("datadir", default="/scratch/clarkf/pems/district4/",
+            help="Location of the raw station txt.gz files")
+
+    parser.add_argument("I80_fname",
+            default="/scratch/clarkf/pems/I80/I80_davis.txt.gz",
+            help="File to append all matching rows to")
+
+    args = parser.parse_args()
+
+    main(args)
