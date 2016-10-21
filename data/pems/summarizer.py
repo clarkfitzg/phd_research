0ba783a8573e4bd4762b2c4807a5264710d3cdcb
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-10-20 17:29:45 -0700

    some more work on transport stuff

--- data/pems/summarizer.py
+++ data/pems/summarizer.py
@@ -18,8 +18,28 @@ I80 = read_station_raw(datadir + "I80_davis.txt")
 I80["timestamp"] = pd.to_datetime(I80["timestamp"],
         infer_datetime_format=True)
 
+I80["weekday"] = I80["timestamp"].dt.dayofweek
+I80["hour"] = I80["timestamp"].dt.hour
+I80["minute"] = I80["timestamp"].dt.minute
+
+I80["second"] = I80["timestamp"].dt.second
+
+secs = I80["second"].value_counts()
+# This shows that the natural way to separate is into groups
+# TODO
+# {0, 1, 
+
 # 5 is the max number of lanes.
 # nullcols = I80.isnull().all(axis=0)
 
 I80["totalflow"] = (I80[["flow" + str(1 + i) for i in range(8)]]
                     .sum(axis=1, skipna=True))
+
+station_summary = pd.DataFrame({
+        "maxflows": I80["totalflow"].groupby(I80["ID"]).max()
+        })
+
+# Likely more useful...
+station_summary["flow99"] = I80["totalflow"].groupby(I80["ID"]).quantile(0.99)
+
+I
