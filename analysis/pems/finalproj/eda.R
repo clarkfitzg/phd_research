c1acce5c3d5f5d282b94f4d40867591f65bddd28
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-11-01 16:47:49 -0700

    starting more seriously on final project

---
+++ analysis/pems/finalproj/eda.R
@@ -0,0 +1,12 @@
+# Tue Nov  1 16:33:13 PDT 2016
+#
+# Exploring PeMS data again. This time for a final project.
+# Goals:
+#
+# - Interesting analysis
+# - Use all data on server
+# - R parallel programming
+# - Use C/C++ for speed
+
+
+d = pems::read30sec("~/data/pems/district3/d03_text_station_raw_2016_04_06.txt.gz")
