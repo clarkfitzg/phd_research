d40dca6c34a74413dde25aaebc8b57e9080f8cfb
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-11-04 12:06:49 -0700

    working towards simplest possible bottlneck detection

--- analysis/pems/finalproj/eda.R
+++ analysis/pems/finalproj/eda.R
@@ -8,5 +8,60 @@
 # - R parallel programming
 # - Use C/C++ for speed
 
+# Basic cleaning steps
+############################################################
 
 d = pems::read30sec("~/data/pems/district3/d03_text_station_raw_2016_04_06.txt.gz")
+
+vals = d[, !(colnames(d) %in% c("timestamp", "ID"))]
+
+badvals = (vals == 0) | is.na(vals)
+
+badrows = apply(badvals, 1, all)
+
+d = d[-badrows, ]
+
+# The distribution of occupancy 1 and 2
+############################################################
+
+occ1 = d$occupancy1[!is.na(d$occupancy1)
+                    & d$occupancy1 != 0
+                    ]
+
+occ2 = d$occupancy2[!is.na(d$occupancy2)
+                    & d$occupancy2 != 0
+                    ]
+
+# Shows that occupancy is distributed about like a mixture of an
+# exponential and a point mass near 1. Could also maybe use beta instead of
+# exponential to get it to stay in [0, 1].
+
+par(mfrow=c(1, 2))
+hist(occ1)
+hist(occ2)
+
+
+# Merge in station metadata
+############################################################
+
+station = read.table("~/data/pems/district3/d03_text_meta_2016_03_18.txt"
+                     , sep = "\t", header = TRUE)
+
+d = merge(d, station[, c("ID", "Abs_PM", "Fwy", "Dir")])
+
+
+# Check for bottlenecks
+# Starting with the simplest things that I can possibly do.
+############################################################
+
+time_thresh = 5  # Units in minutes
+low_occ_thresh = 0.2
+high_occ_thresh = 0.3
+
+occ = d[, grep("occupancy", colnames(d), value = TRUE)]
+mean_occ = rowMeans(occ, na.rm = TRUE)
+
+d$low_occ = mean_occ < low_occ_thresh
+d$high_occ = mean_occ > high_occ_thresh
+
+
