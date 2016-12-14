06d5b2a993db588ee28959db7db14587963f201a
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-12-14 15:59:46 -0800

    about to write the real likelihood

--- analysis/vecchia/examples.jl
+++ analysis/vecchia/examples.jl
@@ -1,3 +1,7 @@
+include("vecchia.jl")
+
+using PyPlot
+
 # Seed RNG
 srand(37)
 n = 100
@@ -5,15 +9,18 @@ n = 100
 # Create covariance matrix to simulate from
 # More typically this might be a Matérn covariance matrix
 Sigma_float = eye(n) + ones(n, n)
-Sigma = PDMat(Sigma_float)
+Sigma = PDMat(Sigma_float)  # Not needed
 mu = zeros(n)
 
 bigmv = MvNormal(mu, Sigma)
+
+bigmv2 = MvNormal(mu, Sigma_float)
 
 x = rand(n)
 
 # Side note:
 # Disappointing that I can't write this: Sigma[1:3, 1:3]
+# Because not for end 
 
 # Testing
 l2 = logpdf_from_slice(x, Sigma_float, 1:3, 4:7)
@@ -22,3 +29,55 @@ l2 = logpdf_from_slice(x, Sigma_float, 1:3, 4:7)
 l_true = logpdf(bigmv, x)
 
 l_approx = vecchia_blockwise(x, Sigma_float)
+
+# P. 6 of paper:
+# when working with small covariance matrices, the computational demand is
+# often dominated by filling in the entries of the covariance matrix rather
+# than factoring the matrix.
+
+# Wed Dec 14 14:52:19 PST 2016
+# Working with Ethan
+
+srand(37)
+ac(dx, rho) = exp(-rho * abs(dx))
+x = -1:0.001:1
+n = length(x)
+rhotrue = 100.
+Sigma = ac.(x .- x', rhotrue)
+ch = chol(Sigma)'
+data = ch * randn(n)
+
+plot(x, data, ".")
+
+testSigma = Symmetric(ac.(x .- x', 100))
+
+logpdf_from_slice(data, testSigma, 1:10, 11:20)
+
+vecchia_blockwise(data, testSigma)
+bigmv2 = MvNormal(mu, Sigma_float)
+
+x = rand(n)
+
+# Side note:
+# Disappointing that I can't write this: Sigma[1:3, 1:3]
+# Because not for end 
+
+# Testing
+l2 = logpdf_from_slice(x, Sigma_float, 1:3, 4:7)
+
+# Evaluate log likelihood
+l_true = logpdf(bigmv, x)
+
+
+
+lltrue = Float64[]
+llapprox = Float64[]
+rhotest = 80:120
+
+for r in rhotest
+    Sigma_r = ac.(x .- x', r)
+    push!(lltrue, vecchia_blockwise(data, Sigma_r))
+    push!(llapprox, vecchia_blockwise(data, Sigma_r))
+end
+
+plot(ll)
--- analysis/vecchia/vecchia.jl
+++ analysis/vecchia/vecchia.jl
@@ -8,9 +8,11 @@ using Gallium
 function logpdf_conditional(x1, x2, Sigma11, Sigma22, Sigma12)
     # log(p(x1|x2)) Straight out of Wikipedia
     Sigma21 = transpose(Sigma12)
-    Sigma22inv = inv(Sigma22)
-    mu_cond = Sigma12 * Sigma22inv * x2
-    Sigma_cond = PDMat(Sigma11 - Sigma12 * Sigma22inv * Sigma21)
+    #Sigma22inv = inv(Sigma22)
+    mu_cond = Sigma12 * (Sigma22 \ x2)
+    Sigma_cond = Sigma11 - Sigma12 * (Sigma22 \ Sigma21)
+    # Hacking
+    Sigma_cond = 0.5 * (Sigma_cond + Sigma_cond')
     dist_cond = MvNormal(mu_cond, Sigma_cond)
     return logpdf(dist_cond, x1)
 end
@@ -20,9 +22,9 @@ function logpdf_from_slice(x, Sigma, s1, s2)
     # Helper to use slices on larger x, Sigma
     x1 = x[s1]
     x2 = x[s2]
-    S11 = Sigma_float[s1, s1]
-    S22 = Sigma_float[s2, s2]
-    S12 = Sigma_float[s1, s2]
+    S11 = Sigma[s1, s1]
+    S22 = Sigma[s2, s2]
+    S12 = Sigma[s1, s2]
     logpdf_conditional(x1, x2, S11, S22, S12)
 end
 
@@ -47,32 +49,41 @@ function vecchia_blockwise(x, Sigma, blocksize = 7)
     # The first block may be smaller than the others
     n1 = rem(n, blocksize)
     if n1 != 0
-        x0 = MvNormal(PDMat(Sigma[1:n1, 1:n1]))
+        x0 = MvNormal(Sigma[1:n1, 1:n1])
         logpdf1 = logpdf(x0, x[1:n1])
     else
         logpdf1 = 0
     end
 
-    stops = n1 + blocksize * collect(0:nblocks)
+    stops = n1 + blocksize * (0:nblocks)
 
-    # TODO
-    # Something strange happens here. Originally I defined this
-    # incorrectly, then I fixed it and ran the code again. Yet the original
-    # remains in the REPL?
-    # slicer(i) = max(1, i - blocksize):i
+    # Alternate way
+    #n1:blocksize:n
+
     slicer(i) = max(1, i - blocksize + 1):i
+
     slices = map(slicer, stops)
+
+    slices2 = map(i -> max(1, i - blocksize + 1):i, stops)
+
 
     zipped = zip(slices[1:nblocks], slices[2:(nblocks+1)])
 
     # Is there a more idiomatic way to apply partial args aka curry?
     # Also I baked in the tuple unpacking...
-    function lpdf(ss)
-        logpdf_from_slice(x, Sigma, ss[1], ss[2])
-    end
+    #function lpdf(ss)
+        #logpdf_from_slice(x, Sigma, ss[1], ss[2])
+        # Python:
+        #logpdf_from_slice(x, Sigma, *ss)
+        #logpdf_from_slice(x, Sigma, ss...)
+    #    print(ss...)
+    #end
+
+    logpdfs = map(ss -> logpdf_from_slice(x, Sigma, ss[1], ss[2]), zipped)
 
     # Parallelism should happen at this step:
-    logpdfs = map(lpdf, zipped)
+    #logpdfs = map(lpdf, zipped)
+
     return logpdf1 + sum(logpdfs)
 
 end
