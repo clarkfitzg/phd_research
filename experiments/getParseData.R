d2258b7d2418a28a990877a9fcfcbff2e6913593
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-02-11 11:00:17 -0800

    notes on edges

--- experiments/getParseData.R
+++ experiments/getParseData.R
@@ -1,6 +1,18 @@
 ex = parse(text = "
-           a = 10
-           b = 20
+           a <- 10
+           b <- a + 5
+           plot(a)
 ")
 
+# Two expressions here
+
 p = getParseData(ex)
+
+symbols = p[p$token == "SYMBOL", "text"]
+
+p[p$token == "SYMBOL", ]
+
+# So how can we tell if assignment is happening?
+# It should be on the LHS of the equal sign...
+# Looks like this returned parse tree is in order. So we can potentially
+# use this to see which symbols are assigned to.
