bcb2a7b6f2dee8f766b86650999fdb2cb2a4bfb6
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-02-16 11:03:36 -0800

    library issue on Teresas code

--- codedepends/FridayHarbor2016-Simulation-Group/taskgraph_sim.R
+++ codedepends/FridayHarbor2016-Simulation-Group/taskgraph_sim.R
@@ -8,3 +8,14 @@ frags = readScript(fname)
 
 # This is similar to the change point- most of the action happens inside
 # one function, frags[11].
+
+# Not currently working:
+#
+#> g = makeNumberTaskGraph(fname)
+#Error in find.package(pkgName, lib.loc, verbose = verbose) :
+#  there is no package called ‘Matrix (>= 1.1.1)’
+#
+# I have Matrix_1.2-8
+#
+# Why? The only two libraries used- lme4 and MASS load up fine. So it must
+# be a problem in CodeDepends.
