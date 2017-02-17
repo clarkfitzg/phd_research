385571321f52215bc8be05dbaaa4fe8f07ab7686
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-02-17 09:02:28 -0800

    minimal example of the dollars

---
+++ codedepends/experiments/analyze_dollars.R
@@ -0,0 +1,11 @@
+# Fri Feb 17 08:57:20 PST 2017
+#
+# When do we get self referential loops with the $ operator?
+
+source("../helpers.R")
+
+tg = makeNumberTaskGraph("dollars.R")
+
+plot(tg)
+
+
---
+++ codedepends/experiments/dollars.R
@@ -0,0 +1,4 @@
+# Here I would expect a single edge from node 1 to 2.
+# But there's an additional edge from node 2 to itself.
+l = list(a = 1)
+l$b = l$a + 5
