414ea0a81c35eb1dc284c9d27399db5c967f3c0f
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-02-22 17:42:09 -0800

    idea for fix

--- codedepends/experiments/analyze_dollars.R
+++ codedepends/experiments/analyze_dollars.R
@@ -10,4 +10,15 @@ tg = CodeDepends::makeTaskGraph("dollars.R")
 
 plot(tg)
 
+frags = readScript("dollars.R")
+info = as(frags, "ScriptInfo")
 
+# Isolating the issues
+# l is used as a variable in each place
+# This just gets the 2nd
+
+# These both get l
+getVariables(info[[1]])
+getVariables(info[[2]])
+
+getPropagateChanges("l", info, index = TRUE)
---
+++ codedepends/experiments/analyze_single_update.R
@@ -0,0 +1,22 @@
+# Wed Feb 22 17:40:38 PST 2017
+# One way to get around this may be to loop over indices and examine the
+# code after a certain loop index.
+
+fname = "single_update.R"
+
+tg = CodeDepends::makeTaskGraph(fname)
+
+plot(tg)
+
+frags = readScript(fname)
+info = as(frags, "ScriptInfo")
+
+# Isolating the issues
+# l is used as a variable in each place
+# This just gets the 2nd
+
+# These both return x, 
+getVariables(info[[1]])
+getVariables(info[[2]])
+
+getPropagateChanges("x", info, index = TRUE)
