3f5cc814ec63f671f29da86fb6f3dffd000f7afe
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-03-22 12:03:45 -0700

    fix bugs in Poisson split

--- proposal/fundamental_diagram/splitter.R
+++ proposal/fundamental_diagram/splitter.R
@@ -1,14 +1,14 @@
 # Run this on Poisson
 
-library(traffic)
+library("traffic", lib.loc = "~/dev/traffic")
 
 do_one = function(fname)
 {
     d = read30sec(fname, nrows = -1)
-    split_write(d, d$ID, "/scratch/...")
+    split_write(d, d$ID, "/scratch/clarkf/pems/stationID")
     message(fname, "completed")
 }
 
-all_files = list.files("/scratch/...")
+all_files = list.files("/scratch/clarkf/pems/district4", full.names = TRUE)
 
 lapply(all_files, do_one)
