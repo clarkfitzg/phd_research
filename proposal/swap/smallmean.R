62ebf241cee52814b1f1edf202a663d789e60c22
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-03-29 17:44:50 -0700

    timings for small n

--- proposal/swap/smallmean.R
+++ proposal/swap/smallmean.R
@@ -1,26 +1,44 @@
 # Wed Mar 29 14:45:18 PDT 2017
 
-# TODO: Use microbenchmark
-
 library(microbenchmark)
 
-n = as.integer(exp(1:10))
-n = rep(n, each = 3)
+n = 1:200
 
-lapply(n, function(n_i){
-    # Triggers garbage collection
-    time = system.time({x <- rnorm(n_i); xbar <- mean(x)}, gcFirst = TRUE)
-    output = data.frame(n = n_i, time = time["elapsed"])
-    write.table(output, "smalltimings.csv", append = TRUE, row.names = FALSE, col.names = FALSE)
+times = lapply(n, function(n_i){
+    # garbage collection affects benchmarks
+    gc()
+    times = microbenchmark(mean(rnorm(n_i)))
+    data.frame(n = n_i, time = times$time)
 })
 
+times = do.call(rbind, times)
 
-timings = read.table("smalltimings.csv"
-                     , col.names = c("n", "seconds"))
+# Units are microseconds
+times$time = times$time / 1000
 
+smalltime = by(times$time, times$n, quantile)
+smalltime = as.data.frame(do.call(rbind, smalltime))
+smalltime = cbind(as.integer(row.names(smalltime)), smalltime)
+colnames(smalltime) = c("n", "min", "q25", "median", "q75", "max")
 
-with(timings, plot(n, seconds
-                   , main = "Time to compute mean(rnorm(n))"
+with(smalltime, plot(n, median
+                   , main = "median time to compute mean(rnorm(n))"
+                   , ylab = expression(paste("time (", mu, "s)"))
                    ))
 
+lines(smalltime$n, smalltime$q25, lty = 2)
 
+fit = lm(time ~ n, data = times)
+
+# Throw out the obvious outliers
+fit2 = lm(time ~ n, data = times[times$time < 20, ])
+
+summary(fit2)
+
+confint(fit2)
+#                  2.5 %     97.5 %
+# (Intercept) 4.20876198 4.25000930
+# n           0.06412455 0.06448087
+#
+# For this simple task there's a fixed overhead of ~4.2 microseconds and an
+# additional 64 nanoseconds for each additional n.
