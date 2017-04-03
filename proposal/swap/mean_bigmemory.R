f7c546d70bd9dc79b02c511c255b872309b16338
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-04-03 10:28:52 -0700

    bigmemory program ready to run

--- proposal/swap/mean_bigmemory.R
+++ proposal/swap/mean_bigmemory.R
@@ -10,12 +10,16 @@ SIZE_DOUBLE = 8
 frac_memory = seq(from = 0.1, to = 2, by = 0.1)
 frac_memory = rep(frac_memory, each = 3)
 n = frac_memory * MEMORY_GB * 1e9 / SIZE_DOUBLE
+
+# We could try to tune this chunk size, but because this operation is
+# linear it won't matter.
 n_chunk = n[1]
 
 # Write as we go in case of crashing
 for (i in seq_along(frac_memory)){
     # Triggers garbage collection
     time = system.time({
+        # Probably could do this in parallel.
         ncol = n[i] / n_chunk
         x = filebacked.big.matrix(nrow = n_chunk, ncol = ncol, backingfile = "")
         for (j in seq(ncol)){
