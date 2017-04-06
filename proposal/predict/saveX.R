a2dd35107e6ec427b660c1bb0625298176e5560b
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-04-06 10:32:08 -0700

    write X.csv to ssd

--- proposal/predict/saveX.R
+++ proposal/predict/saveX.R
@@ -1,3 +1,7 @@
+# Rscript saveX.R 1e6
+#
+# 1e6 will make chunks ~ 24 MB in size
+#
 # Fast IO is important here
 library(data.table)
 Rprof("saveX.out")
@@ -6,16 +10,24 @@ NCHUNK = commandArgs(trailingOnly = TRUE)
 NCHUNK = as.integer(NCHUNK)
 
 set.seed(312)
-nleft = 1e9
+ntotal = 1e9
+nleft = ntotal
 p = 3L
+xfile = "/ssd/clarkf/X.csv"
+
+if(file.exists(xfile)){
+    stop("X.csv is already there.")
+}
 
 while(nleft > 0){
-    n_i = max(NCHUNK, nleft)
+    n_i = min(NCHUNK, nleft)
     nleft = nleft - n_i
 
     X = rnorm(n_i * p)
     # Sneaky matrix coercian
-    dim(X) = c(n, p)
+    dim(X) = c(n_i, p)
     Xdt = data.table(X)
-    fwrite(Xdt, "/ssd/X.csv", append = TRUE, col.names = FALSE)
+    fwrite(Xdt, xfile, append = TRUE, col.names = FALSE)
+
+    message(sprintf("%.4g percent remaining", 100 * nleft / ntotal))
 }
