8401306cf6f5897a5973fec91f45d860203093b6
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-05-05 15:38:48 -0700

    going to try on server

--- proposal/fraud/benford.R
+++ proposal/fraud/benford.R
@@ -20,15 +20,39 @@ cc[keep] = c("factor", "factor", "numeric")
 itcont = read.table("~/data/itcont.txt"
            , sep = "|"
            , colClasses = cc
-           , skip = 300
+           , nrows = 300
            )
-colnames(itcont) = keepers
+#colnames(itcont) = keepers
 
-
+# line 270 doesn't have 21 elements
 
 itcont = read.table("~/data/itcont.txt"
            , sep = "|"
-           , nrows = 500
+           , colClasses = cc
+           , skip = 200
+           , nrows = 300
+           )
+# skip = 10, fails on line 260, consistent with above
+# skip = 50, fails on line 220, consistent with above
+# skip = 100, works fine 
+
+
+itcont = data.table::fread("~/data/itcont.txt", sep = "|", nrows = 500)
+
+
+lines = readLines("~/data/itcont.txt", n = 500)
+
+ls = strsplit(lines, "|", fixed =TRUE)
+
+unique(sapply(ls, length))
+
+ls[[270]]
+
+
+# This works. Inexplicably.
+itcont = read.table("~/data/bad3.txt"
+           , sep = "|"
+           , nrows = 300
            )
 
 colnames(itcont) = keepers
