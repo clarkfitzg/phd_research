645c527479a25dd88e4960ca11303f0d23e9d1fa
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-05-17 11:39:00 -0700

    include stuff on expression dependency graph

--- proposal/compute_times/plot_times.R
+++ proposal/compute_times/plot_times.R
@@ -1,16 +1,16 @@
 # Rough timings, based on various experiments. Units are seconds
 
-overhead = list("simple_function_call" = 300 * 1e-9
-               , "serialize_local_socket" = 8000 * 1e-9
-               , "fork_evaluation" = 1.5 * 1e-3
-               , "start_R_interpreter" = 150 * 1e-3
+overhead = list("Baseline" = 300 * 1e-9
+               , "Serialize" = 8000 * 1e-9
+               , "Fork" = 1.5 * 1e-3
+               , "Startup" = 150 * 1e-3
                )
 overhead = do.call(c, overhead)
 
 pdf("overhead.pdf", height = 4)
 barplot(overhead
         , log = "y"
-        , cex.names = 0.7
+        #, cex.names = 0.7
         , main = "fixed sources of overhead"
         , ylab = "seconds"
         , ylim = c(1e-7, 1)
