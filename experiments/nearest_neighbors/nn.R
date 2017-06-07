826b02d22a7a73782929c5ee25ebf11eef9165f6
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-07 11:49:34 -0700

    parallel version of nearest neighbors

--- experiments/nearest_neighbors/nn.R
+++ experiments/nearest_neighbors/nn.R
@@ -3,6 +3,8 @@
 # 141C last homework was to parallelize a version of nearest neighbors
 # Let's see what it would take to do it automatically in R
 
+library(parallel)
+
 
 distance = function(a, b)
     # The Euclidean distance between vectors a and b
@@ -16,7 +18,8 @@ nn = function(z, x)
     # Returns a vector of indices
 {
 
-    # Nested apply could probably be more clearly expressed as a for loop
+    # Pairwise distances for every row of x and z
+    # for loop would probably be more clear.
     d = apply(z, 1, function(zrow){
             apply(x, 1, function(xrow){
                       distance(xrow, zrow)
@@ -26,6 +29,41 @@ nn = function(z, x)
 }
 
 
+nnpar = function(z, x)
+    # Fork Parallel version
+    #
+{
+
+    # I know I'm on a machine that can fork(), so I only pass the indices
+    # in as arguments. This beats passing the entire matrix around.
+    indices = parallel::splitIndices(nrow(z), 2)
+    force(x)
+
+    # Translate the apply into mclapply
+    dl = parallel::mclapply(indices, function(idx){
+        d = apply(z[idx, ], 1, function(zrow){
+                apply(x, 1, function(xrow){
+                          distance(xrow, zrow)
+    })})})
+    d = do.call(cbind, dl)
+
+    # Didn't use multiprocessing here because I knew this line was fast
+    apply(d, 2, which.min)
+}
+
+
+apply_parallel = function(code)
+    # Experiment... how to generalize the above translation?
+{
+    # Split initial data
+
+    # Evaluate in parallel
+
+    # Recombine result
+
+    # Call remainder of code
+}
+
 # Test it with the same dimensions as the homework problem
 ############################################################
 
@@ -34,5 +72,8 @@ dims = c(10000, 109)
 x = matrix(rnorm(prod(dims)), ncol = dims[2])
 z = matrix(rnorm(prod(1000, dims[2])), ncol = dims[2])
 
-# Takes 60 seconds. Much better than naive Python version
+# Takes 45 seconds. Much better than naive Python version
 system.time(nbrs <- nn(z, x))
+
+# 25 seconds
+system.time(nbrs2 <- nnpar(z, x))
