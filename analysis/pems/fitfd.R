e2ba55f6ae953ca6690ec5b7bb3a5716d6d48d7e
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-26 16:33:21 -0700

    results from processing on server

--- analysis/pems/fitfd.R
+++ analysis/pems/fitfd.R
@@ -1,6 +1,15 @@
 # Mon Jun 26 11:22:02 PDT 2017
 #
 # Process all the PEMS data
+#
+# First version took 63 minutes to run on 20 cores. No optimization was done.
+# Note that this is almost the same amount of time spent reading and
+# splitting the data in the first place.
+#
+# First optimization would be to store the grouped data more efficiently,
+# for example in a fast columnar binary format.
+# Second optimization is an early out for fd_rlm if all the
+# observations are zero or NA.
 
 fd_rlm = function(flow, occupancy, cutoff = 0.15, ...)
 {
@@ -24,8 +33,13 @@ process_one = function(station_filename)
 }
 
 
-allfiles = list.files("/scratch/clarkf/pems/TODO")
+allfiles = list.files("/scratch/clarkf/pems/stationID", full.names = TRUE)
 
-fds = parallel::mclapply(allfiles, function(x) try(process_one(x)))
+fds = parallel::mclapply(allfiles, function(x) try(process_one(x)),
+                         mc.cores = 20L)
 
 save.image("fds.RData")
+
+
+# About half resulted in some kind of error.
+errors = sapply(fds, is, "try-error")
