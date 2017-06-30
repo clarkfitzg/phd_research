9cd5bda083dea2f9978a3401a533bf138631f2a8
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-30 11:44:03 -0700

    strange 3rd cluster comes from type = OR

---
+++ analysis/pems/premapping.R
@@ -0,0 +1,28 @@
+# Fri Jun 30 11:18:10 PDT 2017
+#
+# Determine the latitude and longitude for the stations so that they can be
+# plotted.
+
+station_cluster = read.csv("station_cluster.csv")
+
+stations = read.table("~/data/pems/stations.txt", header = TRUE
+                      , sep = "\t", quote = "")
+
+station_cluster = merge(station_cluster
+        , stations[, c("ID", "Latitude", "Longitude", "Type", "Lanes")]
+        )
+
+
+# The below shows that nearly all detectors of type "OR", which I believe
+# corresponds to on ramp, are in the strange 3rd cluster. Also detectors of
+# type "FR". Behavior on an on ramp should be quite a bit different than on
+# the normal freeway. So I should go back and restrict the clustering just
+# to the mainline stations.
+
+cluster3 = station_cluster$cluster == 3
+s12 = station_cluster[!cluster3, ]
+s3 = station_cluster[cluster3, ]
+
+plot(s12$Type)
+
+plot(s3$Type)
