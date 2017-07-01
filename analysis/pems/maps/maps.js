6f8b9573e6ef7fcd9cc83dde176bf20efdba9bd4
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-06-30 17:52:09 -0700

    clusters.html

--- analysis/pems/maps/maps.js
+++ analysis/pems/maps/maps.js
@@ -9,17 +9,33 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 }).addTo(map);
 
 // From https://github.com/pointhi/leaflet-color-markers
-var greenIcon = new L.Icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
+var violetIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png'
+  , iconSize: [13, 21]
+  , iconAnchor: [6, 21],
 });
 
+var orangeIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png'
+  , iconSize: [13, 21]
+  , iconAnchor: [6, 21],
+});
+
+var greenIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png'
+  , iconSize: [13, 21]
+  , iconAnchor: [6, 21],
+});
+
+
 for (i = 0; i < station.ID.length; i++) {
-    L.marker([station.Latitude[i], station.Longitude[i]], {icon: greenIcon}).addTo(map);
+    if (station.cluster[i] == 1) {
+        L.marker([station.Latitude[i], station.Longitude[i]], {icon: violetIcon}).addTo(map);
+    }
+    if (station.cluster[i] == 2) {
+        L.marker([station.Latitude[i], station.Longitude[i]], {icon: orangeIcon}).addTo(map);
+    }
+    if (station.cluster[i] == 3) {
+        L.marker([station.Latitude[i], station.Longitude[i]], {icon: greenIcon}).addTo(map);
+    }
 }
-
-
