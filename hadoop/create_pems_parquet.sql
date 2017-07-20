46fb74362b5bd95a53b31a59785e3c76cbaf95dd
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-07-20 10:01:37 -0700

    kicking off the SQL insert with the cluster clause

--- hadoop/create_pems_parquet.sql
+++ hadoop/create_pems_parquet.sql
@@ -3,6 +3,12 @@
 --SET hive.exec.max.dynamic.partitions=100000;
 --SET hive.exec.max.dynamic.partitions.pernode=10000;
 
+-- Surprising that the CREATE TABLE statement does not enforce the
+-- bucketing when it's written to. Reference:
+-- https://cwiki.apache.org/confluence/display/Hive/LanguageManual+DDL+BucketedTables
+
+SET hive.enforce.bucketing = true;
+
 DROP TABLE pems_parquet;
 
 CREATE EXTERNAL TABLE pems_parquet (
@@ -16,9 +22,13 @@ CREATE EXTERNAL TABLE pems_parquet (
     , flow7 INT, occupancy7 DOUBLE, speed7 DOUBLE
     , flow8 INT, occupancy8 DOUBLE, speed8 DOUBLE
 )
+COMMENT "This is Clark writing a comment to see where it shows up."
 CLUSTERED BY (station) INTO 256 BUCKETS
 STORED AS PARQUET
-LOCATION '/user/clarkf/pems_parquet';
+LOCATION '/user/clarkf/pems_parquet'
+;
 
 INSERT INTO TABLE pems_parquet
-SELECT * FROM pems;
+SELECT * FROM pems
+CLUSTER BY station
+;
