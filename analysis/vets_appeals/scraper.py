49be6ce92f13ff5016726ce6e43507bbcc974c45
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-08-04 17:43:19 -0700

    vet data scraper

--- analysis/vets_appeals/scraper.py
+++ analysis/vets_appeals/scraper.py
@@ -2,7 +2,37 @@
 Scraping vets appeals data.
 """
 
-baseurl = "https://www.index.va.gov/search/va/bva_search.jsp"
+from lxml import etree
+import requests
 
-url2 = "https://www.index.va.gov/search/va/bva_search.jsp?RPP=50&DB=2016&RS=51"
-url3 = "https://www.index.va.gov/search/va/bva_search.jsp?RPP=50&DB=2016&RS=101"
+
+def download(RS = "1", datadir = "/home/clark/data/vets/appeals/"):
+    """
+    Download and save 50 of the VA appeals using links provided by the
+    search engine.
+    """
+    baseurl = "https://www.index.va.gov/search/va/bva_search.jsp"
+    response = requests.get(baseurl, params = {"RPP": "50", "RS": RS})
+    html = etree.HTML(response.text)
+    links = html.xpath('//*[@id="results-area"]/div/a/@href')
+
+    for link in links:
+        response = requests.get(link)
+        fname = datadir + link.split("/")[-1]
+        with open(fname, "w") as f:
+            f.write(response.text)
+
+
+def main():
+    """
+    Attempt to download everything.
+    """
+    total_results = 961333
+    for RS in range(1, total_results, 50):
+        RS = str(RS)
+        download(RS)
+        print("downloaded {}".format(RS))
+
+
+if __name__ == "__main__":
+    main()
