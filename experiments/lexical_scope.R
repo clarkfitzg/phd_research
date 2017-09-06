5f6977a9727dc420774252ae42d1c73762361d57
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-09-06 11:49:27 -0700

    learning about lexical scope

--- experiments/lexical_scope.R
+++ experiments/lexical_scope.R
@@ -20,3 +20,73 @@ g = function(){
 # If this prints "inside g" it's dynamic.
 # If "global" then lexical.
 g()
+
+
+############################################################
+
+f = function() y
+
+g = function(){
+    y = 20
+    f()
+}
+
+# calling g() fails to find y:
+g()
+
+# Is there a way to make f find y?
+
+ffactory = function(){
+    function() y
+}
+
+g = function(){
+    f = ffactory()
+    y = 20
+    f()
+}
+
+g()
+
+# This doesn't find y, since the parent environments for f come from
+# ffactory, then the global environment.
+# How about modifying the environment?
+
+f = function() y
+
+g = function(){
+    y = 20
+    f()
+}
+
+
+# Wed Sep  6 11:31:29 PDT 2017
+# Actually what I really want is to construct a call and evaluate it
+# somewhere else.
+
+f = function(y) y + 1
+
+call = as.call(list(as.name('f'), as.name('y')))
+
+trace_func = function() eval(call)
+
+g = function(){
+    y = 20
+    trace_func()
+}
+
+# Doesn't find y because of lexical scoping
+g()
+
+
+# Now something trickier...
+
+#trace_func2 = function() browser()
+trace_func2 = function() eval(call, parent.frame(1L))
+
+g2 = function(){
+    y = 20
+    trace_func2()
+}
+
+g2()
