d93bdeb3870c0c8f47060de0a971bee8d78492e2
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-17 10:16:51 -0700

    working on R UDFs in hive

---
+++ hadoop/weekday/pid.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+# Tue Oct 17 10:04:00 PDT 2017
+#
+# Not removing whitespace as in the Python version.
+
+sep = "\t"
+n = 100000
+
+tbl = read.table(stdin(), sep = sep)
+
+pid = Sys.getpid()
+
+tbl = cbind(tbl, pid)
+
+write.table(tbl, stdout(), sep = sep)
---
+++ hadoop/weekday/pidr.sql
@@ -0,0 +1,22 @@
+DROP TABLE movie_r_pid;
+
+CREATE TABLE movie_r_pid (
+  userid INT,
+  movieid INT,
+  rating INT,
+  pid INT)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
+
+add FILE pid.R;
+
+INSERT OVERWRITE TABLE movie_r_pid
+SELECT
+  TRANSFORM (userid, movieid, rating)
+  USING 'Rscript pid.R'
+  AS (userid, movieid, rating, pid)
+FROM u_data;
+
+SELECT pid, COUNT(*)
+FROM movie_r_pid
+GROUP BY pid;
