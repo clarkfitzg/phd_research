fce8a777413297fe65292e6180904e17dbd23d68
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-19 08:57:55 -0700

    gamma function example working

--- hadoop/basic/gamma.R
+++ hadoop/basic/gamma.R
@@ -1,8 +1,11 @@
 #!/usr/bin/env Rscript
 
-args = commandArgs(trailingOnly=TRUE)
-sep = args[1]
-col = as.integer(args[2])
+#args = commandArgs(trailingOnly=TRUE)
+#sep = args[1]
+#col = as.integer(args[2])
+
+sep = "\t"
+col = 3L
 
 infile = file("stdin")
 tbl = read.table(infile, sep = sep)
@@ -10,4 +13,5 @@ tbl = read.table(infile, sep = sep)
 gam = gamma(tbl[, col])
 tbl = cbind(tbl, gam)
 
-write.table(tbl, stdout(), sep = sep)
+write.table(tbl, stdout(), sep = sep
+            , col.names = FALSE, row.names = FALSE)
--- hadoop/basic/gamma.sql
+++ hadoop/basic/gamma.sql
@@ -17,12 +17,13 @@ INSERT OVERWRITE TABLE gamma
 SELECT
   TRANSFORM (userid, movieid, rating)
 -- The tab and 3 following the command makes them available from commandArgs within R
-  USING "Rscript gamma.R $'\t' 3"
+  --USING "Rscript gamma.R $'\t' 3"
+  USING "Rscript gamma.R"
   AS (userid, movieid, rating, gamma_col)
 FROM u_data
 ;
 
-SELECT rating, gamma_col, COUNT(*)
+SELECT *
 FROM gamma
-GROUP BY rating, gamma
+LIMIT 10
 ;
