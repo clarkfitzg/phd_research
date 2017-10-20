b04e3c321fdbf8c03a3b0dbf0bdb2897b18aa934
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-19 17:18:45 -0700

    next step: log to stderr

--- hadoop/basic/log.R
+++ hadoop/basic/log.R
@@ -1,10 +1,15 @@
 #!/usr/bin/env Rscript
 tryCatch({
 
-
 infile = file("stdin")
 
 tbl = readLines(infile)
+
+# I don't see this anywhere on the Hadoop worker nodes.
+# Maybe I can log directly to hdfs?
+# Or is there a more robust way to read from stderr?
+logfile = "/home/clarkf/hiveR.log"
+writeLines("hot damn it's a log!", logfile)
 
 stop("R error dude!")
 
