18c4fe7ff559e096c7a1dba9d92626b7698ad043
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-29 08:09:46 -0700

    having trouble with outer() type operations

--- analysis/pems/hadoop/innerproduct.R
+++ analysis/pems/hadoop/innerproduct.R
@@ -24,6 +24,8 @@ fd = read.table("~/data/pems/fd.tsv"
  
 # TODO: Clean
 
+length(unique(fd$station)) == length(fd$station)
+
 sapply(fd, function(x) mean(is.na(x)))
 
 # These points define the fd
@@ -73,10 +75,43 @@ fd_func = function(row)
     }
 }
 
+fd_inner = function(f1, f2 = f1)
+{
+    f1f2 = function(x) f1(x) * f2(x)
+    integrate(f1f2, lower = 0, upper = 1)$value
+}
+class(fd_inner) = c("kernel", class(fd_inner))
+
+f_1 = fd_func(fd[1, ])
+
+fd_inner(f_1)
+
 x = seq(0, 1, by = 0.01)
 
-f1 = fd_func(fd[1, ])
+# All looks reasonable
+points(x, f_1(x))
 
-points(x, f1(x))
+fd$funcs = by(fd, fd$station, fd_func, simplify = FALSE)
+fd$funcs = do.call(list, fd$funcs)
+
+
+# Works fine
+fd_inner(fd$funcs[[1]], fd$funcs[[2]])
+
+
+# Build the kernel matrix
+
+library(kernlab)
+
+m = as.matrix(fd$funcs)
+
+# Not working
+km = kernelMatrix(fd_inner, as.matrix(fd$funcs))
 
 ?kernlab::kkmeans
+
+# Do it the dumb way.
+# No, this produces the same error
+# Maybe it's the list that causes the issue?
+d = outer(fd$funcs, fd$funcs, fd_inner)
+
