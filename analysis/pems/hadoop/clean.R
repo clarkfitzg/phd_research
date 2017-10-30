b9e2e1c525f89a83b1eafadc3da16bce168f7045
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-10-30 12:00:41 -0700

    starting to look more closely at the data

---
+++ analysis/pems/hadoop/clean.R
@@ -0,0 +1,82 @@
+col.names = c("station"
+, "n_total"
+, "n_middle"
+, "n_high"
+, "left_slope"
+, "left_slope_se"
+, "mid_intercept"
+, "mid_intercept_se"
+, "mid_slope"
+, "mid_slope_se"
+, "right_slope"
+, "right_slope_se"
+)
+
+colClasses = c("integer"
+, "integer"
+, "integer"
+, "integer"
+, "numeric"
+, "numeric"
+, "numeric"
+, "numeric"
+, "numeric"
+, "numeric"
+, "numeric"
+, "numeric"
+)
+
+fd = read.table("~/data/pems/fd.tsv", na.strings = "NULL"
+    , col.names = col.names, colClasses = colClasses)
+
+
+n_original = nrow(fd)
+ 
+length(unique(fd$station)) == length(fd$station)
+
+sapply(fd, function(x) mean(is.na(x)))
+
+# These points define the fd
+fd$leftx = fd$mid_intercept / (fd$left_slope - fd$mid_slope)
+fd$lefty = fd$left_slope * fd$leftx
+
+fd$rightx = (fd$mid_intercept + fd$right_slope) /
+                    (fd$right_slope - fd$mid_slope)
+fd$righty = fd$mid_intercept + fd$mid_slope * fd$rightx
+
+ordered = with(fd, (0 <= leftx) & (leftx <= rightx) & (rightx <= 1))
+ordered[is.na(ordered)] = FALSE
+
+# 0.87 have the right order. Good!
+mean(ordered)
+
+fd = fd[ordered, ]
+
+fd$mid_slope_se = as.numeric(fd$mid_slope_se)
+fd$right_slope_se = as.numeric(fd$right_slope_se)
+
+par(mfrow = c(2, 2))
+
+lapply(fd[, c("left_slope_se", "mid_slope_se", "right_slope_se")], hist)
+
+lapply(fd[, c("left_slope", "mid_slope", "right_slope")], quantile)
+
+# Apply some reasonable cutoffs
+reasonable = (fd$left_slope < 300) &
+    (-100 < fd$right_slope) &
+    (fd$left_slope_se < 1)
+
+mean(reasonable)
+
+fd = fd[reasonable, ]
+
+par(mfrow = c(1, 1))
+
+nrow(fd)
+
+fd$right_convex = fd$mid_slope < fd$right_slope
+
+mean(fd$right_convex)
+
+write.table(fd, "~/data/pems/fdclean.tsv", sep = "\t"
+            , row.names = FALSE)
--- analysis/pems/hadoop/innerproduct.R
+++ analysis/pems/hadoop/innerproduct.R
@@ -1,60 +1,13 @@
 # Fri Oct 27 16:41:18 PDT 2017
 #
+# Run this after clean.R
+#
 # Thinking about the kernel trick and computing the distance between
 # functions.
 #
 # This is \int_0^1 f(x) g(x) dx
 
-cnames = c("station"
-, "n_total"
-, "n_middle"
-, "n_high"
-, "left_slope"
-, "left_slope_se"
-, "mid_intercept"
-, "mid_intercept_se"
-, "mid_slope"
-, "mid_slope_se"
-, "right_slope"
-, "right_slope_se"
-)
-
-fd = read.table("~/data/pems/fd.tsv"
-    , col.names = cnames)
- 
-# TODO: Clean
-
-length(unique(fd$station)) == length(fd$station)
-
-sapply(fd, function(x) mean(is.na(x)))
-
-# These points define the fd
-fd$leftx = fd$mid_intercept / (fd$left_slope - fd$mid_slope)
-fd$lefty = fd$left_slope * fd$leftx
-
-fd$rightx = (fd$mid_intercept + fd$right_slope) /
-                    (fd$right_slope - fd$mid_slope)
-fd$righty = fd$mid_intercept + fd$mid_slope * fd$rightx
-
-ordered = with(fd, (0 <= leftx) & (leftx <= rightx) & (rightx <= 1))
-ordered[is.na(ordered)] = FALSE
-
-# 0.87 have the right order. Good!
-mean(ordered)
-
-fd = fd[ordered, ]
-
-fdlines = function(row)
-{
-    with(row, abline(0, left_slope, col = "blue"))
-    with(row, abline(mid_intercept, mid_slope, col = "blue"))
-    with(row, abline(-right_slope, right_slope, col = "blue"))
-    with(row, lines(c(0, leftx, rightx, 1), c(0, lefty, righty, 0)
-                    , lwd = 2))
-}
-
-plot(c(0, 1), c(0, 25), type = "n")
-fdlines(fd[1, ])
+fd = read.table("~/data/pems/fdclean.tsv", header = TRUE)
 
 # Return a vectorized function
 fd_func = function(row)
