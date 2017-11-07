7f6bb2ec238360224b77d3317c2eca9b59722271
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-11-07 08:47:12 -0800

    first clustering

--- analysis/pems/hadoop/eda3.R
+++ analysis/pems/hadoop/eda3.R
@@ -9,9 +9,17 @@ d = read.table("~/data/pems/fd_inner.txt")
 
 dm = as.matrix(d)
 
-# Scaling it into a correlation matrix
+nonzero = diag(dm) != 0
+
+dm = dm[nonzero, nonzero]
+
+# Scaling a similarity matrix into a correlation matrix
 dcor = dm
-N = nrow(d)
+N = nrow(dm)
+# This is a nice use case for when a for loop is easy to write, but an
+# apply function would be awkward.
+# I didn't know why I was getting NA's, so I wanted to stop and check when
+# it occurs.
 for(i in 1:N){
     for(j in 1:N){
         dij = dm[i, j] / sqrt(dm[i, i] * dm[j, j])
@@ -20,30 +28,24 @@ for(i in 1:N){
     }
 }
 
-
-sum(is.na(dm))
-
-sum(is.na(dcor))
-
-hist(diag(d3))
-
-
-# Don't know what's going on. Why are values of dcor NA?
-
-
-
-
-
-# They're almost all the same :)
-hist(dcor[dcor > 0.5])
-
-N2 = sum(is.na(dcor[1, ]))
-
-d2 = dcor[!is.na(dcor)]
-
 # Becomes a measure of dissimilarity
 D = as.dist(1 - dcor)
 
-D[is.na(D)] = NULL
 
 pam1 = pam(D, 2)
+
+clusters = pam1$clustering
+
+table(clusters)
+
+# What do these representative FD's look like?
+pam1$medoids
+
+fd = read.table("~/data/pems/fdclean.tsv", header = TRUE)
+fd = fd[nonzero, ]
+
+# match clusters to stations
+c2 = clusters[as.character(fd$station)]
+fd$cluster = c2
+
+# The representative clusters
