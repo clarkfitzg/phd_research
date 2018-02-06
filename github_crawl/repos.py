54e98bfccc4cfacc29bec977db77378af024449b
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-02-05 17:21:35 -0800

    scraping R repos with Python

--- github_crawl/repos.py
+++ github_crawl/repos.py
@@ -1,41 +1,61 @@
 import requests
-
 import json
+import time
 
-baseurl = "https://api.github.com"
 
 
-def download(search = "data analysis", license = "mit", language = "R"):
+def download(url = None, page = 1, search = "data analysis", license = "mit", language = "R"):
     # Can only have five logical operators
 
     query = " ".join((search,
         "NOT assignment",
-        "NOT package",
         "NOT workshop",
         "NOT tutorial",
         "NOT coursera",
+        "NOT course",
         "language:{} license:{}".format(language, license),
         "fork:false mirror:false"))
 
-    response = requests.get(baseurl + "/search/repositories",
-            {"q": query, "per_page": 100, "page": page})
+    if url is None:
+        r = requests.get("https://api.github.com" + "/search/repositories",
+                {"q": query, "per_page": 100, "page": page})
+    else:
+        r = requests.get(url)
 
-    
+    return {"links": r.links,
+            "repos": r.json()["items"],
+            }
 
-page = 1
 
-licenses = ("mit", "gpl")
-r["total_count"]
+if __name__ == "__main__":
 
-r["items"][99]["forks"]
+    r = download()
+    lasturl = r["links"]["last"]["url"]
+    repos = r["repos"]
 
-l = response.headers["Link"]
-len(r["items"])
+    while(True):
+        time.sleep(1)
+        try:
+            r = download(r["links"]["next"]["url"])
+            repos += r["repos"]
+        except KeyError:
+            break
 
-[x["name"] for x in r["items"]]
 
-[x["description"] for x in r["items"]]
-
-nd2 = [x["name"] for x in r["items"]]
-
-print(nd)
+# 
+# r = download()
+# 
+# r1 = r[1]
+# 
+# licenses = ("mit", "gpl")
+# r["total_count"]
+# 
+# len(r["items"])
+# 
+# [x["name"] for x in r["items"]]
+# 
+# [x["description"] for x in r["items"]]
+# 
+# nd2 = [x["name"] for x in r["items"]]
+# 
+# print(nd)
