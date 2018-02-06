eb07e1e0d3ff53b60f55ffff377e9f570048f3c5
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-02-06 15:28:35 -0800

    Im redirecting stderr, but this may not be accurate

--- github_crawl/explore.sh
+++ github_crawl/explore.sh
@@ -7,10 +7,22 @@ dd="/home/clark/data/code/r_data_analysis"
 find $dd -path "*.R" -type f | wc -l
 
 # Which libraries are being used?
-# Something around 843
+# 843 calls, 201 different libraries.
+# And not a single call to 
 find $dd -path "*.R" -type f | \
-    xargs grep "library\('?\K(\S+)'?\)" --no-filename -oP | \
-    tr -d '")\' | \
+    xargs grep "library\(\K(\S+)\)" --no-filename -oP 2> /dev/null | \
+    tr -d \'\"')' | \
+    sort | \
+    uniq -c | \
+    sort -n | \
+    #wc -l | \
+    grep "ggplot" | \
+    cat
+
+
+# How often are the apply families used?
+find $dd -path "*.R" -type f | \
+    xargs grep "\K([sltv]apply|Map|Reduce|mclapply)" -oP | \
     sort | \
     uniq -c | \
     sort -n | \
