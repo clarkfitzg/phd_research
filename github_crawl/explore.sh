85c2d64d1182a02c6774bd05c9b7d22cd0064ede
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-02-06 16:16:22 -0800

    just a couple repos using Reduce

--- github_crawl/explore.sh
+++ github_crawl/explore.sh
@@ -18,20 +18,50 @@ find $dd -path "*.R" -type f -print0 | \
     #grep "parallel" | \
     cat
 
+
 # Which ones use parallel?
 find $dd -path "*.R" -type f -print0 | \
     xargs -0 grep "library\([\"']?parallel" -oP | \
     cut  -d "/" -f 7 | \
-    sort -n | \
+    sort | \
     uniq | \
     cat
 
+#BRC_SingleCell_LJames
+#DBDA2Eprograms
+#DBDA2E-utilities
+#gpseq-seq-gg
+#MetaAnalysisJaegerEngelmannVasishth2017
+#seq-lib-gg
 
-# How often are the apply families used?
-find $dd -path "*.R" -type f | \
-    xargs grep "\K([sltv]apply|Map|Reduce|mclapply)" -oP | \
+apply="\K([sltv]apply|Map|Reduce|mclapply)" 
+
+# How often are the apply families used? fairly common.
+find $dd -path "*.R" -type f -print0 | \
+    xargs -0 grep $apply -oP --no-filename | \
     sort | \
     uniq -c | \
     sort -n | \
-    #wc -l | \
+    cat
+
+
+# Which repos use them?
+# 152 out of 301. Half of them.
+find $dd -path "*.R" -type f -print0 | \
+    xargs -0 grep $apply -oP | \
+    cut  -d "/" -f 7 | \
+    sort | \
+    uniq | \
+    wc -l | \
+    cat
+
+
+# Which repos use Reduce?
+# 9 out of 301.
+find $dd -path "*.R" -type f -print0 | \
+    xargs -0 grep "Reduce\(" -oP | \
+    cut  -d "/" -f 7 | \
+    sort | \
+    uniq | \
+    wc -l | \
     cat
