c9757bac711a8238279483e08871f22c056872b8
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-02-14 10:40:36 -0800

    R's base::merge appears to be pretty slow compared to data.table

--- experiments/merge/times.R
+++ experiments/merge/times.R
@@ -17,12 +17,16 @@ item <- data.frame(item = readRDS(paste0(DATADIR, "item/item"))
 
 # 9.3 seconds to load 1.6 GB object from disk
 # Implies disk IO speed around 170 MB/second
-# Also 2.2 seconds to load from memory
 io_speed = 1600 / 9.3
+# Also 2-3 seconds to load from memory. Why does it take this long?
+# Because that's how long it takes to copy a 1.6 GB object in memory, even
+# from within an R process.
 
 order <- data.frame(item = readRDS(paste0(DATADIR, "order/order"))
     , order = readRDS(paste0(DATADIR, "order/customer"))
     )
 
-tmerge = system.time(item <- merge(item, order))
+o2 = order[1:1000, ]
+
+tmerge = system.time(item <- merge(item, o2))
 
