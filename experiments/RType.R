7060b5f47e26f7b55e0361285048cd80ce22a3ba
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-03-29 15:39:20 -0700

    VectorType commented out of vignette

--- experiments/RType.R
+++ experiments/RType.R
@@ -2,16 +2,44 @@
 # Working through this thread: https://github.com/duncantl/RTypeInference/issues/5
 
 library(RTypeInference)
+library(typesys)
 
-dyncut = function(x, ...) c(-Inf, 0, Inf)
+# Thu Mar 29 14:58:08 PDT 2018
+#
+# First let's try something basic. This is from 
+# RTypeInference test_infer_dm
 
-npbin = function(x)
+  node = rstatic::quote_cfg(
+    function(x) {
+      y = f(x)
+      g(y)
+    }
+  )
+
+  global_tenv = typesys::TypeEnvironment$new(
+    "f" = a ~ a,
+    "g" = Integer ~ Logical,
+    quantify = TRUE
+  )
+
+  result = infer_dm(node, global_tenv)
+
+  # -----
+  expect_is(result@args[["x_1"]], "typesys::IntegerType")
+  expect_is(result@return_type, "typesys::LogicalType")
+
+
+
+############################################################
+
+
+npbin = function(occupancy, flow, station)
 {
-    breaks = dyncut(x$occupancy2, pts_per_bin = 200)
-    binned = cut(x$occupancy2, breaks, right = FALSE)
-    groups = split(x$flow2, binned)
+    breaks = dyncut(occupancy)
+    binned = cut(occupancy, breaks, right = FALSE)
+    groups = split(flow, binned)
 
-    out = data.frame(station = rep(x[1, "station"], length(groups))
+    out = data.frame(station = rep(station, length(groups))
         , right_end_occ = breaks[-1]
         , mean_flow = sapply(groups, mean)
         , sd_flow = sapply(groups, sd) 
@@ -23,6 +51,10 @@ npbin = function(x)
 
 cfg = rstatic::to_cfg(npbin)
 
-cons = constrain(cfg)
+tenv = typesys::TypeEnvironment$new(
+"dyncut" = VectorType(NumericType())
+, quantify = TRUE
+)
 
-infer_dm(cfg)
+
+infer_dm(cfg, tenv)
