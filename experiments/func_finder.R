89a2dadaf097470130468ef3ec06a2c5d8205adb
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-03 16:45:01 -0700

    function finder working

--- experiments/func_finder.R
+++ experiments/func_finder.R
@@ -3,12 +3,13 @@
 # I need a function that looks at a function inside a package and returns
 # the name and package of all other functions that it calls. Then I can
 # call this recursively.
+#
+# It appears that I need to be looking through the namespace internals.
 
 # Which functions in the stats package have the fewest expressions?
 
 p = "package:stats"
 
-names = ls(p, all.names = TRUE)
 
 funs = sapply(names, function(x) is.function(get(x, p)))
 
@@ -30,9 +31,39 @@ library(CodeDepends)
 # - symbols which aren't functions
 # - functions defined in the bodies of functions
 
+# Return a data frame of objects imported into a package
+package_funcs = function(pkg_name)
+{
+
+    # Inferring the class of objects would be very nice to have here because
+    # then I know which method will be called.
+    ns = getNamespace(pkg_name)
+
+    this_package = data.frame(name = names(ns))
+    this_package$package = pkg_name
+
+    imports = getNamespaceImports(pkg_name)
+    imports = imports[names(imports) != "base"]
+    package_names = names(imports)
+
+    each_package = vector(length(imports), mode = "list")
+
+    for(i in seq_along(imports)){
+        tmp = data.frame(name = names(imports[[i]]))
+        tmp$package = package_names[i]
+        each_package[[i]] = tmp
+    }
+
+    each_package = c(list(this_package), each_package)
+
+    do.call(rbind, each_package)
+
+}
+
 fun = stats::dgamma
-pkg = "package:stats"
+pkg = "stats"
 
 func_names = names(getInputs(body(fun))@functions)
 
 
+common = intersect(func_names, names(ns))
