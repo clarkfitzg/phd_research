2d753f1c7528aa2fcc9d752546dbfa214591ee36
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-03 12:26:07 -0700

    find examples of simple functions in stats package

---
+++ experiments/func_finder.R
@@ -0,0 +1,23 @@
+# Tue Apr  3 12:07:04 PDT 2018
+#
+# I need a function that looks at a function inside a package and returns
+# the name and package of all other functions that it calls. Then I can
+# call this recursively.
+
+# Which functions in the stats package have the fewest expressions?
+
+p = "package:stats"
+
+names = ls(p, all.names = TRUE)
+
+funs = sapply(names, function(x) is.function(get(x, p)))
+
+funs = names[funs]
+
+len = sapply(funs, function(x) length(body(get(x, p))))
+
+len = sort(len)
+
+head(len[len > 2])
+
+# complete.cases is an appealing one- it only calls .External
