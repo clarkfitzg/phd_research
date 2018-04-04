1e274332e3600a0a16e3d66d2d73368eaadbf0b4
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-04 14:54:41 -0700

    function finder seems to work

--- experiments/func_finder.R
+++ experiments/func_finder.R
@@ -5,6 +5,9 @@
 # call this recursively.
 #
 # There are several namespace related functions that make this easy.
+#
+# This would be useful to explore the package ecosystem- it can show which
+# are the most and least used functions in packages.
 
 # Which functions in the stats package have the fewest expressions?
 
@@ -28,10 +31,14 @@ head(len[len > 3])
 
 library(CodeDepends)
 
+# Eventually we'll need to cache all this for performance reasons.
+#
 # Ignoring the following issues for the moment:
 #
 # - symbols which aren't functions
 # - functions defined in the bodies of functions
+# - Explicit use of base::func in package code
+
 
 # Return a data frame of objects imported into a package
 package_funcs = function(pkg_name)
@@ -63,26 +70,30 @@ package_funcs = function(pkg_name)
 }
 
 
-# Which functions in which packages does fun use directly?
-resolve_names = function(fun, pkg)
+base_funcs = data.frame(name = names(getNamespace("base")))
+base_funcs$package = "base"
+
+
+# Helper to combine package and base functions
+package_and_base_funcs = function(pkg, .base_funcs = base_funcs)
 {
+    pkgfuncs = package_funcs(pkg)
 
-    func_names = names(getInputs(body(fun))@functions)
+    # It's possible for a package to override a base function internally.
+    bfuncs = .base_funcs[!(.base_funcs$name %in% pkgfuncs$name), ]
 
-    base_funcs = data.frame(name = names(getNamespace("base")))
-    base_funcs$package = "base"
+    rbind(pkgfuncs, bfuncs)
+}
 
-    all_funcs = package_funcs("stats")
 
-    # Ideally I'd like to do some kind of chained lookups here. I can probably
-    # accomplish this with the right kind of joins.
+# Which functions in which packages does fun use directly?
+used_funcs = function(fun, pkg)
+{
+    used = data.frame(name = names(getInputs(body(fun))@functions))
 
-    # TODO: Pop these from func_names
-    common = intersect(func_names, all_funcs$name)
+    possible = package_and_base_funcs(pkg)
 
-    keepers = func_names %in% base_funcs$name
-
-    resolved = base_funcs[base_funcs$name %in% func_names, ]
+    merge(used, possible, by = "name", all.x = TRUE)
 }
 
 
@@ -91,10 +102,10 @@ resolve_names = function(fun, pkg)
 
 names(getInputs(body(dgamma))@functions)
 
-f1 = resolve_names("dgamma", "stats")
+f1 = used_funcs("dgamma", "stats")
 
 
 names(getInputs(body(mad))@functions)
 
 # Currently failing because it doesn't find "median"
-f2 = resolve_names("mad", "stats")
+f2 = used_funcs("mad", "stats")
---
+++ notes/literature.md
@@ -0,0 +1,4 @@
+Wed Apr  4 11:51:14 PDT 2018
+
+Reading Duncan's thesis now. Section 4.3 relates to the task parallelism
+that I'm looking at.
