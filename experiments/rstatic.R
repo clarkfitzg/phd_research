5537c05e840a506295ee296bf61a0593131aad82
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-18 20:03:17 -0700

    a few little experiments with rstatic

--- experiments/rstatic.R
+++ experiments/rstatic.R
@@ -11,6 +11,7 @@ node0 = quote_ast({
     x[, "c"] = rnorm(10)
     sum(x$a)
     head(x)
+    x[, "a"]
 })
 
 # Can we add arbitrary attributes to the graph?
@@ -52,3 +53,40 @@ nr = to_r(node)
 f = eval(nr)
 
 f()
+
+# Wed Apr 18 15:03:31 PDT 2018
+# How difficult would it be to build the task graph, ie. use-definition
+# chains, inside here?
+
+# Well the node for the brace has a slot for ID. It's NA here, and I don't know what
+# Nick's plans are for it, but if every object has something that uniquely
+# identifies the nodes then I can make a graph out of it.
+# I get that "address" in the AST with recursive indexing.
+
+# Can I recursively index into the brace like in the AST?
+# No.
+#node0[[c(1, 4)]]
+
+node0[[3]]$fn$namespace
+
+
+
+# It might be possible to do algebra on these things. Maybe.
+node0$body[[3]]$args[[1]]$args
+
+# Might be good to have a missing arg object
+node0$body[[5]]$args[[2]]$name
+
+
+# A simple case of a for loop that should be an apply
+forloop = quote_ast(
+    for(i in seq_along(x)){
+        y[i] = f(x[i])
+    }
+)
+
+# Essentially I'm going to need a tool like this to infer if a for loop is
+# parallel.
+forloop$ivar
+
+forloop$body$body
