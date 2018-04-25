f90fd07e5dadd75c3f3bf6223d2de31137b2fc61
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-25 10:18:40 -0700

    spoke with Duncan

--- experiments/forloop/dependence.R
+++ experiments/forloop/dependence.R
@@ -62,13 +62,18 @@ findvars = function(ast)
 # rstatic does the dispatching that makes it more difficult to do what I
 # want:
 
-e = quote(x[1] <- f(4))
+e = quote(x[i] <- f(4))
 # > a[[1]]
 # `<-`
 # > a[[2]]
 # x[1]
 # > a[[3]]
 # f(4)
+
+names(e) = c("class", "lhs", "rhs")
+
+# This behavior is kind of nice.
+e$lhs
 
 # How difficult is it to apply some of rstatic's extraction operators to
 # objects in the language?
@@ -78,16 +83,18 @@ e = quote(x[1] <- f(4))
     x[[i]]
 }
 
+#`$<-.<-` = function(x, name)
+
 e[[2]]
 
-# fails
+# fails to dispatch on class
 e$write
 
 # TRUE
 class(e) == "<-"
 
 # Explicitly assign
-class(e) = "<-"
+class(e) = class(e)
 
 # Now it works
 e$write
