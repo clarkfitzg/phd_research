4ac41d5f2b00e8317699b0c0aa5241bdc18e9700
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-27 16:33:42 -0700

    creating the lapply

--- experiments/forloop/loop_to_par.R
+++ experiments/forloop/loop_to_par.R
@@ -14,12 +14,15 @@ for(i in x) {
 findLocals(l7)
 
 
+# Will end up using better tools to get these in here.
+nameloop = function(loop){
+    names(loop) = c("for", "iter", "ivar", "body")
+    loop
+}
+
+
 loop_body_to_func = function(loop)
 {
-
-    # Probably will end up using better tools to get these in here.
-    names(loop) = c("for", "iter", "ivar", "body")
-
     f = function() NULL
 
     args = alist(ITER_VARIABLE = )
@@ -34,7 +37,21 @@ loop_body_to_func = function(loop)
 }
 
 
+# This might actually work with a shared object like a bigmemory matrix
+loop_to_lapply_local_assign = function(loop)
+{
+    func = loop_body_to_func(loop)
+
+    call("lapply", loop$ivar, func)
+}
+
+
+
+l7 = nameloop(l7)
+
 f7 = loop_body_to_func(l7)
+
+la7 = loop_to_lapply_without_assign(l7)
 
 
 # This doesn't find the variable `ans`.
