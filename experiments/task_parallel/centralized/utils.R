4429e0030ac0cd096f866c28b81ab970f84bd731
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-05-03 09:55:04 -0700

    starting out with Rmpi

--- experiments/task_parallel/centralized/utils.R
+++ experiments/task_parallel/centralized/utils.R
@@ -1,3 +1,14 @@
+library(Rmpi)
+
+# Can't do more than 1 slave on my 2 core machine!
+mpi.spawn.Rslaves(nslaves = 1)
+
+# Here's what I need to be using.
+mpi.send.Robj
+
+
+mpi.close.Rslaves()
+
 
 #' Send Command To Worker
 command = function(expr, worker, async = TRUE)
