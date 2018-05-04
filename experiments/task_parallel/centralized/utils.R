ddd245cbb802f7fa00a85e3c4b7794e8362d5d26
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-05-03 17:22:02 -0700

    evaluate is pretty tricky

--- experiments/task_parallel/centralized/utils.R
+++ experiments/task_parallel/centralized/utils.R
@@ -1,8 +1,30 @@
 library(Rmpi)
 
 
+#' Evaluate expression on worker
+evaluate = function(code, worker)
+{
+    id = mpi.comm.rank()
+    if(id == worker){
+        mpi.bcast.cmd(code)
+    }
+    NULL
+}
+
+
+#' Send Object
+send = function(obj, source, dest, tag, comm = 1)
+{
+    id = mpi.comm.rank()
+    if(id == source){
+        mpi.send.Robj(obj, dest, tag, comm)
+    }
+    NULL
+}
+
+
 #' Receive Object
-receive = function(name, tag, source, dest)
+receive = function(name, source, dest, tag, comm = 1)
 {
     id = mpi.comm.rank()
     if(id == dest){
