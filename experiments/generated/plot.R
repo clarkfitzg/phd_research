0ea952799c292fe26353e4e8648a922462f9f117
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-05-09 15:05:31 -0700

    more experiments

--- experiments/generated/plot.R
+++ experiments/generated/plot.R
@@ -1,10 +1,12 @@
 library(autoparallel)
 
-script = parse("script3.R")
-
+script = parse("script5.R")
 tg = task_graph(script)
-
 schedule = minimize_start_time(script, tg, nprocs = 3L)
+plot(schedule)
 
+script = parse("script4.R")
+tg = task_graph(script)
+schedule = minimize_start_time(script, tg, nprocs = 2L)
 plot(schedule)
 
