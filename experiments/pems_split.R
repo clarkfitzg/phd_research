7fd841cdc2b0b63a898fb99caa2725e1e8cce664
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-11-05 17:48:27 -0800

    still got all the columns

--- experiments/pems_split.R
+++ experiments/pems_split.R
@@ -3,9 +3,6 @@
 # Using R's iotools package to split the PEMS data into the required
 # grouping variable, and also filter.
 
-# For testing
-# fname = "~/data/pems/d04_text_station_raw_2016_04_13.txt.gz"
-
 library(iotools)
 
 
@@ -36,11 +33,23 @@ process_file = function(fname, keepers = c("station", "flow2", "occupancy2"))
         # Can't seem to get the row names out of here... Oh well.
         #write.csv.raw(y, yfile, append = TRUE, sep = ",", nsep = ",", col.names = FALSE)
     })
-    col.names[out] = names(columns[keep_col] )
+    colnames(out) = names(columns[keep_col] )
     out
 }
 
 
+if(FALSE)
+{
+
+    testfile = "~/data/pems/d04_text_station_raw_2016_04_13.txt.gz"
+
 system.time(
-    test <- process_file("~/data/pems/d04_text_station_raw_2016_04_13.txt.gz")
+    test <- process_file(testfile)
 )
+
+
+    con = gzfile(testfile, open = "rt")
+
+t1 = read.table(con, sep = ",", nrows = 10)
+
+}
