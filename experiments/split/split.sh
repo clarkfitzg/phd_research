65e5efe225107eb3384a921ed84f5d90c3788148
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-11-06 17:16:44 -0800

    working version of awk file splitter

--- experiments/split/split.sh
+++ experiments/split/split.sh
@@ -3,7 +3,7 @@
 
 # Too many files- about 3500
 # https://stackoverflow.com/questions/32878146/too-many-open-files-error-while-running-awk-command
-awk -F, '{close("experiment_station/"$1)}{print >> "experiment_station/"$1}' threecolumns.csv
+awk -F, '{print >> "experiment_station/"$1; close("experiment_station/"$1)}' threecolumns.csv
 
 #'/pattern here/{close("file"i); i++}{print > "file"i}' InputFile
 
