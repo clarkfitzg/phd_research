2c6bbe36ed533965fe22b311ab6ac74ec6bad8cc
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-11-06 17:43:38 -0800

    2nd try at Python version

--- experiments/split/awk_split.sh
+++ experiments/split/awk_split.sh
@@ -5,5 +5,10 @@
 # https://stackoverflow.com/questions/32878146/too-many-open-files-error-while-running-awk-command
 awk -F, '{print >> "experiment_station/"$1; close("experiment_station/"$1)}' threecolumns.csv
 
+# $ time ~/phd_research/experiments/split/split.sh
+# real    0m48.964s
+# user    0m11.876s
+# sys     0m36.712s
+
 #'/pattern here/{close("file"i); i++}{print > "file"i}' InputFile
 
--- experiments/split/py_split.py
+++ experiments/split/py_split.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import csv
 import itertools
 import operator
@@ -6,10 +8,18 @@ datadir = "/home/clark/data/pems/experiment_station/"
 
 fname = "/home/clark/data/pems/threecolumns_sorted.csv"
 
-with open(fname) as f:
-    reader = csv.reader(f)
-    grouped = itertools.groupby(reader, lambda x: x[0])
-    for grp in grouped:
-        grpfile = datadir + grp[0]
-        with open(grpfile) as g:
-            g.writelines(grp[1])
+def write_group(filename, rows, dd = datadir):
+    full_filename = dd + filename
+    with open(full_filename, "w") as g:
+        writer = csv.writer(g)
+        writer.writerows(rows)
+
+
+f = open(fname)
+reader = csv.reader(f)
+grouped = itertools.groupby(reader, lambda x: x[0])
+for grp in grouped:
+    write_group(*grp)
+
+close(f)
+
