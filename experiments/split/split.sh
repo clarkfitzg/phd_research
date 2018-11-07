0386c0d25f9419281b6c159844e3e3b870f530e9
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-11-06 16:18:10 -0800

    this isnt working either :(

--- experiments/split/split.sh
+++ experiments/split/split.sh
@@ -1,2 +1,9 @@
 # Split into files based on the first key
-awk -F, '{print >> "station/"$1}' sorted_data.txt
+#awk -F, '{print >> "experiment_station/"$1}' threecolumns.csv
+
+# Too many files- about 3500
+# https://stackoverflow.com/questions/32878146/too-many-open-files-error-while-running-awk-command
+awk -F, '{close("experiment_station/"$1)}{print >> "experiment_station/"$1}' threecolumns.csv
+
+#'/pattern here/{close("file"i); i++}{print > "file"i}' InputFile
+
