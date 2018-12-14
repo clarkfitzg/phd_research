179b79272761d5b3d6a82edc497f97a431ed871b
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-12-14 15:35:48 -0800

    exploring usaspending db a bit

--- experiments/usaspending/explore.R
+++ experiments/usaspending/explore.R
@@ -18,3 +18,29 @@ tbls = dbListTables(con)
 count_star("psc")
 
 nrows = sapply(tbls, count_star)
+
+df = data.frame(table = tbls, nrow = unlist(nrows))
+row.names(df) = NULL
+
+df = df[order(df$nrow, decreasing = TRUE), ]
+
+
+head_table = function(tbl, n = 10, connection = con)
+{
+    message(tbl)
+    rs = dbSendQuery(connection, paste0("SELECT * FROM ", tbl, " LIMIT ", n))
+    on.exit(dbClearResult(rs))
+    dbFetch(rs)
+}
+
+examples = lapply(df$table[1:10], head_table)
+names(examples) = df$table[1:10]
+
+names(examples)
+
+# Interesting ones:
+# transaction_normalized
+# transaction_fpds
+# transaction_fabs award_description, cfda_title
+
+names(examples[[10]])
