b74c64de44f015f11bf72685ebbc875b3c31d275
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-12-14 14:25:11 -0800

    counting how large the tables are

--- experiments/usaspending/explore.R
+++ experiments/usaspending/explore.R
@@ -1,7 +1,20 @@
+library(DBI)
+
 library(RPostgreSQL)
 
-conn = dbConnect(drv = dbDriver("PostgreSQL")
-    , dbname = "postgres"
-    , host = "localhost"
-    , port = 5432
-    )
+
+con = dbConnect(drv = dbDriver("PostgreSQL"))
+
+count_star = function(tbl, connection = con)
+{
+    message(tbl)
+    rs = dbSendQuery(connection, paste0("SELECT COUNT(*) FROM ", tbl))
+    on.exit(dbClearResult(rs))
+    dbFetch(rs)
+}
+
+tbls = dbListTables(con)
+
+count_star("psc")
+
+nrows = sapply(tbls, count_star)
