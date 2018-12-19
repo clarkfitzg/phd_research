079dca82e38f5e1f49c746b90abe29c99380231b
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-12-19 13:59:45 -0800

    hateful unicode dashes.

--- experiments/usaspending/materialize_views.sh
+++ experiments/usaspending/materialize_views.sh
@@ -1,6 +1,5 @@
 #### Dump Variables ####
-WD=/home/usaspending
-DUMP=/scratch/clarkf/usaspending/usaspending-db-dump-dir
+DUMP=/scratch/clarkf/usaspending/dump
 
 # =========================================================================
 # ==== Comment or remove below if you do not want to materialize views ====
@@ -8,25 +7,40 @@ DUMP=/scratch/clarkf/usaspending/usaspending-db-dump-dir
 #### Materialized View Refresh ####
 # Create list of ONLY materialized views data to refresh
 
-pg_restore ‐‐list $DUMP | grep "MATERIALIZED VIEW DATA" > $WD/refresh.list
+# Something seems to be going wrong when I copy and paste versus when I
+# type these out in the terminal. Could it be some weird unicode character
+# messing things up?
+
+pg_restore --list $DUMP | grep "MATERIALIZED VIEW DATA" > refresh.list
 
 # Refresh materialized view data
 pg_restore \
-‐‐jobs 16 \
-‐‐verbose \
-‐‐exit‐on‐error \
-‐‐use‐list $WD/refresh.list \
+--jobs 16 \
+--verbose \
+--exit-on-error \
+--use-list refresh.list \
 $DUMP
 
+# The above doesn't actually populate the materialized views.
+# Could be a difference in the version of Postgres for the instructions and
+# the version we are using here.
+
+# To actually populate them we need to run:
+
+# REFRESH MATERIALIZED 
+
+# Helpful answer to show which are the materialized views.
+# https://stackoverflow.com/questions/23092983/is-there-a-postgres-command-to-list-drop-all-materialized-views
+
 # Do an additional ANALYZE on the materialized views after being materialized
-pg_restore ‐‐list $DUMP \
+pg_restore --list $DUMP \
 | grep "MATERIALIZED VIEW DATA" \
 | awk '{ print "ANALYZE VERBOSE", $8";" };' \
-> $WD/analyze_matviews.sql
+> analyze_matviews.sql
 
 psql \
-‐‐echo‐all \
-‐‐set ON_ERROR_STOP=on \
-‐‐set VERBOSITY=verbose \
-‐‐set SHOW_CONTEXT=always \
-‐‐file $WD/analyze_matviews.sql
+--echo-all \
+--set ON_ERROR_STOP=on \
+--set VERBOSITY=verbose \
+--set SHOW_CONTEXT=always \
+--file analyze_matviews.sql
