40404473b3e8d0840a5c1e1a6b4548b21abe7786
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-12-28 11:02:07 -0800

    nothing coming to stdout

--- experiments/slurm/stream_find_word.R
+++ experiments/slurm/stream_find_word.R
@@ -1,6 +1,13 @@
-NLINES = 1000L
+# Reimplement bash grep in R, demonstrating how to process a stream.
 
-WORD = "education"
+args = commandArgs(trailingOnly = TRUE)
+
+WORD = args[1]
+if(is.na(WORD)) stop("Pass the word to search for as the first argument.")
+
+NLINES = args[2]
+if(is.na(NLINES)) NLINES = 1000L
+
 
 input = file("stdin")
 open(input)
--- experiments/slurm/stream_find_word.sh
+++ experiments/slurm/stream_find_word.sh
@@ -2,5 +2,9 @@ COLUMN=description
 COLNUM=16
 WORD=education
 INFILE=/home/clarkf/data/transaction_normalized.csv
-OUTFILE=/home/clarkf/data/
+OUTFILE=/home/clarkf/data/${COLUMN}-${WORD}.txt
+
+head ${INFILE} \
+    | cut -d , -f 16 \
+    | Rscript stream_find_word.R grant
 
