1b4b2c785f98cba99b98d48ca29a1ec17c225e41
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-01-02 10:04:52 -0800

    saving groups

--- experiments/usaspending/awards.R
+++ experiments/usaspending/awards.R
@@ -15,7 +15,24 @@ system.time(awards <- dbFetch(rs))
 
 dbDisconnect(con)
 
+awards$funding_agency_id[is.na(awards$funding_agency_id)] = 0L
+
+# Save it split into groups
+system.time(
+by(awards, awards$funding_agency_id, function(group){
+    datadir = "/scratch/usaspending/awards"
+    fname = paste0(datadir, "/", group[1, "funding_agency_id"], ".csv")
+    write.csv(group, fname, row.names = FALSE)
+})
+)
+
 # Exploring
+
+# Did any fail to match in the join?
+failmatch = awards[(is.na(awards$funding_agency_name) & awards$funding_agency_id != 0), ]
+
+# Only agency 1474, whatever that is.
+table(failmatch$funding_agency_id)
 
 # 6 GB
 print(object.size(awards), units = "GB")
@@ -26,4 +43,4 @@ agencies = table(awards$funding_agency_name)
 agencies = sort(agencies, decreasing = TRUE)
 
 # scaled
-a2 = agencies / sum(agencies)
+a2 = agencies / nrow(awards)
