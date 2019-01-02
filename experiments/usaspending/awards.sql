931166dc1e273e7d8a19a5c828bb74a0ba9ac642
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-01-01 17:49:53 -0800

    awards table is saved to disk

--- experiments/usaspending/awards.sql
+++ experiments/usaspending/awards.sql
@@ -1,6 +1,6 @@
---\copy (Select * From foo) To '/tmp/test.csv' With CSV
-\copy
-(
+DROP TABLE awards_with_agency;
+
+CREATE TEMP TABLE awards_with_agency AS
 SELECT a.id
     , a.total_obligation
     , a.period_of_performance_start_date
@@ -9,9 +9,7 @@ SELECT a.id
     , a.funding_agency_id
     , s.name AS funding_agency_name
 FROM awards AS a
-JOIN subtier_agency AS s
+LEFT JOIN subtier_agency AS s
 ON a.funding_agency_id = s.subtier_agency_id
 -- LIMIT 20
---;
-)
-To '/scratch/clarkf/usaspending/awards.csv' With CSV HEADER
+;
---
+++ experiments/usaspending/notes.md
@@ -0,0 +1,3 @@
+materializing the views took about 24 hours.
+
+The query to join the agency tables, 71 million or so, takes about 1 minute.
