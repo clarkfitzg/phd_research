23882c85dbcd3627cf6c57a1204572473f37dbe9
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-01-01 17:34:12 -0800

    about to try psql version

--- experiments/usaspending/awards.sql
+++ experiments/usaspending/awards.sql
@@ -1,12 +1,17 @@
+--\copy (Select * From foo) To '/tmp/test.csv' With CSV
+\copy
+(
 SELECT a.id
     , a.total_obligation
     , a.period_of_performance_start_date
     , a.description
-    , a.funding_agency_id
     , a.recipient_id
-    , s.name
+    , a.funding_agency_id
+    , s.name AS funding_agency_name
 FROM awards AS a
 JOIN subtier_agency AS s
 ON a.funding_agency_id = s.subtier_agency_id
---LIMIT 10
-;
+-- LIMIT 20
+--;
+)
+To '/scratch/clarkf/usaspending/awards.csv' With CSV HEADER
