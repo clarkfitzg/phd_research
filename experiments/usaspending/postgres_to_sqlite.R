f9b0a456745b0439d595e19320b31d4713c0666c
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-03-01 13:33:39 -0800

    SEEMS to be working to transfer tables between databases

--- experiments/usaspending/postgres_to_sqlite.R
+++ experiments/usaspending/postgres_to_sqlite.R
@@ -1,33 +1,49 @@
 library(DBI)
 library(RPostgreSQL)
 
-CHUNKSIZE = 1e4L
-
 from_db = dbConnect(drv = dbDriver("PostgreSQL"))
 
 to_db = dbConnect(RSQLite::SQLite(), "/scratch/usaspending/usaspending.sqlite")
 
-table_names = data.frame(
-    from = c("transaction_normalized", "recipient_lookup")
 
-    )
+# Copy a table between databases in chunks.
+chunk_copy_table = function(from_name, to_name, .from_db = from_db, .to_db = to_db, chunk_size = 1e4L)
+{
+    # Might be fun to show students what a SQL injection attack is.
+    qstring = paste0("SELECT * FROM ", from_name)
 
-# The stream of incoming data
-# This takes forever => it's NOT streaming
-system.time(res <- dbSendQuery(from_db, "SELECT * FROM universal_transaction_matview"))
+    # The stream of incoming data
+    # This takes 17 minutes for a large table in Postgres
+    #    => it's NOT streaming
+    print(system.time(res <- dbSendQuery(.from_db, qstring)))
+    on.exit(dbClearResult(res))
 
-# Use the first row to initialize things
-first_row = dbFetch(res, n = 1L)
-dbCreateTable(to_db, "transactions", first_row)
-#dbAppendTable(to_db, "transactions", chunk)
+    # Use the first row to initialize things
+    first_row = dbFetch(res, n = 1L)
+    dbCreateTable(.to_db, to_name, first_row)
 
-while(!dbHasCompleted(res)){
-    chunk = dbFetch(res, n = CHUNKSIZE)
-    dbAppendTable(to_db, "transactions", chunk)
+    chunk_index = 1L
+    while(!dbHasCompleted(res)){
+        chunk = dbFetch(res, n = chunk_size)
+        dbAppendTable(.to_db, to_name, chunk)
+
+        message("chunk ", chunk_index)
+        chunk_index = chunk_index + 1L
+    }
+
+    message("completed ", from_name)
 }
 
 
-dbClearResult(res)
+table_names = data.frame(
+    from = c("toptier_agency", "transaction_normalized", "recipient_lookup"),
+    to = c("agency", "transaction", "recipient"),
+    stringsAsFactors = FALSE
+)
+
+
+Map(chunk_copy_table, table_names[, "from"], table_names[, "to"])
+
 
 dbDisconnect(from_db)
 dbDisconnect(to_db)
