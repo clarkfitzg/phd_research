97540ca70e55369de9529682dc71b6074f37bb61
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-07 10:50:28 -0700

    computing this, I see that there are just a bunch of identity matrices

--- experiments/roi.R
+++ experiments/roi.R
@@ -5,16 +5,58 @@
 
 library(ROI)
 
+# for triple_simple_matrix
+library(slam)
+
 
 # Here's for the single GROUP BY
-nworkers = 2L
-ngroups = 5L
+w = 2L
+g = 5L
+gw = g * w
 
-L = L_constraint(L = 
+set.seed(823)
+p = runif(g)
+p = p / sum(p)
+# This will be a linear model more generally
+time_per_group = p
+
+Iw = simple_triplet_diag_matrix(1, nrow = w)
+
+
+############################################################
+# Construct the constraint matrix L using 9 blocks following written notation
+############################################################
+
+L11 = simple_triplet_matrix(
+      i = rep(seq(w), each = g)
+    , j = seq(gw)
+    , v = rep(time_per_group, times = w)
+    )
+
+L12 = -Iw
+
+L13 = simple_triplet_zero_matrix(nrow = w, ncol = 1)
+
+
+make_L21_row = function(i)
+{
+    j = seq(from = i, by = g, length.out = w)
+    ones = rep(1L, length(j))
+    simple_triplet_matrix(i = ones, j = j, v = ones, ncol = gw)
+}
+
+L21_rows = lapply(seq(g), make_L21_row)
+L21 = do.call(rbind, L21_rows)
+
+
+
+Lc = L_constraint(L = 
 )
 
+simple_triplet_matrix
+
 problem = OP(objective = c(rep(0, ngroups), 1L)
-    , constraints = L
+    , constraints = Lc
     , types = c(rep("B", ngroups), "C")
 )
 
