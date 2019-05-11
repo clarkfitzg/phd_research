211fe28736461d995e630789b227a1b3aedcb6ce
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-10 17:24:02 -0700

    ready to implement my algorithm

---
+++ experiments/load_balancing.R
@@ -0,0 +1,15 @@
+# Fri May 10 17:19:10 PDT 2019
+#
+# How to balance n tasks among w workers such that we minimize the time that all workers are finished?
+
+
+# Standard greedy algorithm
+greedy = function(p)
+{
+}
+
+
+# My algorithm, try to fill up workers first plus an epsilon
+full_plus_epsilon = function(p)
+{
+}
