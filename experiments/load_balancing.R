7aac3b9a0c7321286cac6aaf07606e7fb658a027
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-13 11:21:06 -0700

    something is definitely wrong since I'm getting values less than the lower bound

--- experiments/load_balancing.R
+++ experiments/load_balancing.R
@@ -43,7 +43,7 @@ full_plus_epsilon = function(tasktimes, w
 }
 
 
-# make sure thes algorithms work correctly
+# make sure these algorithms work correctly
 tt_hard_1 = c(2, 2, 2, 3, 3)
 tt_hard_2 = c(8, 7, 6, 5, 4)
 
@@ -54,6 +54,8 @@ full_plus_epsilon(tt_hard_2, 2L)
 greedy(tt_hard_2, 2L)
 
 
+# Comparing the performance
+############################################################
 
 maxtime = function(f, tt, w){
     times = lapply(tt, f, w = w)
@@ -61,22 +63,48 @@ maxtime = function(f, tt, w){
 }
 
 
-compare = function(ntasks = 20L, w = 4L, reps = 100L, random_gen = runif
+generate_times = function(ntasks = 20L, w = 2L, random_gen = runif)
+{
+    out = random_gen(ntasks)
+    # Normalize so 1 is the ideal cost
+    out = out * w / sum(out)
+    sort(out, decreasing = TRUE)
+}
+
+
+compare = function(w = 4L, reps = 100L, ...
         , funcs = list(greedy, full_plus_epsilon))
 {
-    tt = lapply(seq(reps), function(...) sort(random_gen(ntasks), decreasing = TRUE))
-    results = sapply(funcs, maxtime, tt = tt, w = w)
-    results
+    tt = lapply(seq(reps), generate_times, w = w, ...)
+    results = lapply(funcs, maxtime, tt = tt, w = w)
+    results = as.data.frame(results)
 }
 
 
 set.seed(23480)
 out = compare(reps = 1000L)
 
-# Positive delta shows that the first approach (greedy) is better.
+# Positive delta shows that the first approach (greedy) is marginally better in this case.
 delta = out[, 1] - out[, 2]
 hist(delta)
 
 t.test(delta)
 
 table(sign(delta))
+
+set.seed(901873)
+out = compare(reps = 1000L, random_gen = function(n) seq(n) + runif(n))
+
+# For this more skew data the new algorithm does better.
+# This suggests that the new algorithm is more robust.
+
+# To compare numbers I'll need to get the scaling right.
+# What we really care about is relatively how far away we are from the lower bound.
+# I.e. one algorithm is 1.05 times what the lower bound is.
+# We can get this easily by scaling the task times so the lower bound is 1.
+
+# What is a typical distribution of group sizes?
+# The PEMS data has uniform group sizes.
+# Often I see something that looks an exponential decay, with a long tail.
+
+
