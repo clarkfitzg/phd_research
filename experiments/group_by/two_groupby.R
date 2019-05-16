48d98253d2f74936f38646100427fa14df15c34e
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-05-16 12:05:13 -0700

    starting to implement two group by algorithm

--- experiments/group_by/two_groupby.R
+++ experiments/group_by/two_groupby.R
@@ -9,7 +9,7 @@
 
 # These are the unit costs in terms of the size of the group / data to compute the group by's and to transfer data.
 # They're only important relative to each other, which is why I put them in this form.
-cost = list(gb1 = 1, gb2 = 1, transfer = 1)
+cost = list(gb1 = 1, gb2 = 1, transfer = 5)
 
 # The best case scenario for the speedup is if the transfer goes from transferring every data element to transferring nothing at all, which would be a speedup of: 
 (cost$gb1 + cost$gb2 + cost$transfer) / (cost$gb1 + cost$gb2)
@@ -25,5 +25,42 @@ w = 3
 set.seed(32180)
 P = matrix(runif(g1 * g2), nrow = g1)
 
-P1 = rowsum(P)
-P2 = colsum(P)
+
+
+# Put groups on the 'best' workers, starting with the largest groups and without exceeding a balanced load by epsilon.
+# The best workers are those that have relatively more of the same second group already on them.
+# Returns an integer vector the same length as P1 that assigns each group to one of the w workers.
+first_group = function(P, w)
+{
+    P1 = rowsum(P)
+    P2 = colsum(P)
+
+    epsilon = min(P1)
+    full_plus_epsilon = sum(P) / w + epsilon
+
+    # These get updated as the assignments are made
+    assignments = rep(NA, length(P1))
+    times = rep(0, w)
+
+    for(idx in order(P1, decreasing = TRUE)){
+        tm = P1[idx]
+        newload = P[idx, ]
+        g2_loads = worker_g2_loads(assignments, P)
+        w = find_best_worker(newload, g2_loads, times, epsilon)
+        assignments[idx] = w
+        times[w] = times[w] + tm
+    }
+
+    assignments
+}
+
+
+    # G2 starts out evenly distributed among workers.
+    # This is necessary instead of zeros for how we'll balance based on inner products.
+    #worker_g2_loads = lapply(seq(w), function(...) P2 / w)
+
+worker_g2_loads = function(assignments, P)
+{
+}
+
+
