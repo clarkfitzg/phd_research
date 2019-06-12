08903daffc19b03d1487e84717211f4fb647d42b
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-06-12 12:39:05 -0700

    recording uses manually, so I can understand what a programmatic approach will have to do

--- experiments/rstatic/split.R
+++ experiments/rstatic/split.R
@@ -12,8 +12,18 @@ library(rstatic)
 large_objects = "pems"
 vector_funcs = c("[", "sin")
 
-name_resource_lookup = "pems"
-resources = list("Details of pems data")
+# Only including the large named data objects at this time.
+# Could extend it to include all the resources we care about- constants, files, etc.
+# Also can use integers as lookup keys rather than names.
+resources = new.env()
+resources[["pems"]] = list("Details of pems data", uses = list())
+
+record_use = function(use, varname, .resources = resources)
+{
+    .resources[[varname]][["uses"]] = c(.resources[[varname]][["uses"]], use)
+}
+
+
 
 
 code = quote_ast({
@@ -22,7 +32,8 @@ code = quote_ast({
 })$contents
 
 
-# Directly annotate them by hand
+# Directly annotate them by hand.
+# This is how the algorithm will proceed.
 
 # The first `pems`
-code[[1]]$read$args$contents[[1]]$resource = 1L
+record_use(code[[1]]$read$args$contents[[1]], "pems")
