4e8ea1e2caaef4c1cee0f8dcf4971351a92be4fe
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-06-19 08:26:40 -0700

    writing what the vector detection should do

---
+++ experiments/rstatic/vector.R
@@ -0,0 +1,17 @@
+source("propagate.R")
+
+pems_id = namer()
+name_resource[["pems"]] = pems_id
+resources[[pems_id]] = list(chunked_object = TRUE)
+
+ast = quote_ast({
+    stn = pems[, "station"]
+    foo = exp(stn) + 3
+    result = by(pems, stn, npbin)
+})
+
+# Does all the inference
+propagate(ast, name_resource, resources, namer, vectorfuncs = c("exp", "+"))
+
+# Should be a chunked object
+get_resource(ast[[2]])
