0315520478e260eba8bbff69bda10de8de50ab20
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-06-20 11:21:26 -0700

    conversion methods for rstatic object

--- experiments/rstatic/vector.R
+++ experiments/rstatic/vector.R
@@ -1,3 +1,4 @@
+
 source("propagate.R")
 
 x_id = namer()
@@ -6,19 +7,40 @@ resources[[x_id]] = list(chunked_object = TRUE)
 
 ast = quote_ast({
     y = x[, "y"]
-    ybar = mean(y)
-    ysd = sd(y)
-    z = (y - ybar) / ysd
+    y2 = 2 * y
 })
 
 # Mark everything with whether it's a chunked object or not.
-propagate(ast, name_resource, resources, namer, vector_funcs = c("exp", "+"))
+propagate(ast, name_resource, resources, namer, vector_funcs = c("exp", "+", "*"))
 
 # Should be a chunked object
 get_resource(ast[[2]], resources)
 
-# Next is the code generation.
-# A good implementation should do the following:
+# Next is the scheduling and code generation.
+# Start with the simplest things possible.
 # - Each worker will read in a chunk of `x`, doing column selection at the source so that only the column 'y' comes through.
-# - Each worker will compute the parts necessary for `ybar` and `ysd` on their chunks of the data.
+# - Each worker will compute `y2` and save it
 
+# Developing stuff that will go into makeParallel
+
+library(makeParallel)
+
+setOldClass("Brace")
+
+setMethod("inferGraph", signature(code = "Brace", time = "missing"),
+    function(code, time, ...){
+        expr = lapply(code$contents, as_language)
+        expr = as.expression(expr)
+        callGeneric(expr, ...)
+})
+
+g = inferGraph(ast)
+
+setClass("VectorSchedule", contains = "Schedule")
+
+
+scheduleVector = function(graph, data)
+{
+    # The idea is to combine as many nodes in the graph as possible.
+    # This means I need to reconcile the dependency graph with the AST.
+}
