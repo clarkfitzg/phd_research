060b864afeeb43d7996fad706091f3c9770b1aee
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-08-02 16:28:23 -0700

    fails

--- experiments/pems/split.sh
+++ experiments/pems/split.sh
@@ -7,4 +7,9 @@ DATAFILE=(~/data/pems/d04_text_station_raw_2016_08_22.txt.gz)
 
 # From https://unix.stackexchange.com/questions/114061/extract-data-from-a-file-and-place-in-different-files-based-on-one-column-value
 
-time gunzip -stdout $DATAFILE | awk -F, '{ print > ($2 ".csv") }' 
+#awk: 400085.csv makes too many open files
+# input record number 18, file
+#  source line number 1
+#
+
+time gunzip --stdout $DATAFILE | awk -F, '{ print > ($2 ".csv") }' 
